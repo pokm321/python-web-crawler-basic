5baa04f4b0886321846b33e2232fc60612f01cfa
pokm321 <pokm90@gmail.com>
2023-04-02 22:02:54 +0900

    Ask for user input to search for the term on both Indeed and WWR

---
+++ extractors/indeed.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+def get_page_count(keyword):
+  options = Options()
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
+  #options.add_experimental_option("detach", True)
+
+  url = f"https://kr.indeed.com/jobs?q={keyword}"
+
+  browser = webdriver.Chrome(options=options)
+  browser.get(url)
+
+  soup = BeautifulSoup(browser.page_source, "html.parser") 
+  pagination = len(soup.find("nav", attrs={"aria-label": "pagination"}).find_all("div", recursive=False))
+  if pagination <= 1:
+    return 1
+  else:
+    return pagination - 1
+
+def extract_indeed_jobs(keyword):
+  results = []
+  pages = get_page_count(keyword)
+
+  for page in range(pages):
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    #options.add_experimental_option("detach", True)
+
+    url = f"https://kr.indeed.com/jobs?q={keyword}&start={page * 10}"
+
+    browser = webdriver.Chrome(options=options)
+    browser.get(url)
+
+    soup = BeautifulSoup(browser.page_source, "html.parser") 
+
+    job_list = soup.find("ul", class_="jobsearch-ResultsList")
+    jobs = job_list.find_all("li", recursive=False)
+
+    for job in jobs:
+      zone = job.find("div", class_="mosaic-zone")
+      if zone == None:
+        anchor = job.select_one("h2 a")
+        title = anchor["aria-label"]
+        link = anchor["href"]
+        company = job.find("span", class_="companyName").string
+        location = job.find("div", class_="companyLocation").string
+        job_data = {
+          'company' : company,
+          'location' : location,
+          'position' : title,
+          'link' : f"https://kr.indeed.com{link}"
+        }
+        results.append(job_data)
+  return results
--- main.py
+++ main.py
@@ -1,39 +1,15 @@
-from requests import get
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
+from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
 
+keyword = input("What do you want to search for?")
 
-options = Options()
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-#options.add_experimental_option("detach", True)
+wwr = extract_wwr_jobs(keyword)
+indeed = extract_indeed_jobs(keyword)
 
-search_term = "python"
-url = f"https://kr.indeed.com/jobs?q={search_term}"
-
-browser = webdriver.Chrome(options=options)
-browser.get(url)
-
-soup = BeautifulSoup(browser.page_source, "html.parser") 
-results = []
-job_list = soup.find("ul", class_="jobsearch-ResultsList")
-jobs = job_list.find_all("li", recursive=False)
+jobs = wwr + indeed
 
 for job in jobs:
-  zone = job.find("div", class_="mosaic-zone")
-  if zone == None:
-    anchor = job.select_one("h2 a")
-    title = anchor["aria-label"]
-    link = anchor["href"]
-    company = job.find("span", class_="companyName").string
-    location = job.find("div", class_="companyLocation").string
-    job_data = {
-      'company' : company,
-      'location' : location,
-      'position' : title,
-      'link' : f"https://kr.indeed.com{link}"
-    }
-    results.append(job_data)
+    print(job)
+    print("//////////////////////////////////////\n//////////////////////////////////////")
 
+print(len(jobs), "jobs found.")
